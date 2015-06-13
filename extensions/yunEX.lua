ba029f81773772bbdd06f98891f892a57e926369
aa3615058 <aa3615058@126.com>
2015-06-14 01:57:56 +0800

    liyunpeng √

--- extensions/yunEX.lua
+++ extensions/yunEX.lua
@@ -22,56 +22,6 @@ lualanyan = sgs.CreateTriggerSkill{
 	end
 }
 
-lualienv = sgs.CreateTriggerSkill{
-	name = "lualienv",
-	frequency = sgs.Skill_Frequent,
-	events = {sgs.Damage,sgs.Damaged,sgs.FinishJudge},
-	view_as_skill = lualienvViewAs, 
-	
-	on_trigger = function(self, event, player, data)
-		local room = player:getRoom()
-		local damage = data:toDamage()
-		local target = nil
-		local flag = nil
-		if event == sgs.Damage then
-			target = damage.to
-			flag = player:getGender() == target:getGender()
-		elseif event == sgs.Damaged then
-			target = damage.from
-			flag = player:getGender() ~= target:getGender()
-		elseif event == sgs.FinishJudge then
-			local judge = data:toJudge()
-			if judge.reason == self:objectName() then
-				local card = judge.card
-				if card:isBlack() then
-					player:obtainCard(card)
-				else
-					if not player:canDiscard(player, "he") then return false end
-					if not room:askForDiscard(player, "lualienv", 1, 1, true, true, "~lualienv") then return false end
-					local players = room:getAlivePlayers()
-					local _player = room:askForPlayerChosen(player, players, self:objectName(), "@lualienv_prompt")
-					local recover = sgs.RecoverStruct()
-					recover.who = player
-					room:recover(_player, recover)
-					--room:askForUseCard(player, "@@lualienv", "@lualienv_prompt")
-				end
-				return false
-			end
-		end
-		
-		if flag then
-			if player:askForSkillInvoke("lualienv") then
-				local judge = sgs.JudgeStruct()
-				judge.good = true
-				judge.reason = "lualienv"
-				judge.who = player
-				room:judge(judge)
-			end
-		end
-		return false
-	end
-}
-
 lualienvCard = sgs.CreateSkillCard{
 	name = "lualienvCard",
 	target_fixed = false,
@@ -102,9 +52,8 @@ lualienvCard = sgs.CreateSkillCard{
 		room:recover(dest, recover)
 	end
 }
-
-lualienvViewAs = sgs.CreateOneCardViewAsSkill{
-	name = "lualienvViewAs", 
+lualienvVS = sgs.CreateOneCardViewAsSkill{
+	name = "lualienv",
 	
 	view_filter = function(self, selected, to_select)
 		return true
@@ -122,10 +71,48 @@ lualienvViewAs = sgs.CreateOneCardViewAsSkill{
 		return pattern == "@@lualienv"
 	end
 }
-
+lualienv = sgs.CreateTriggerSkill{
+	name = "lualienv",
+	frequency = sgs.Skill_Frequent,
+	events = {sgs.Damage, sgs.Damaged, sgs.FinishJudge},
+	view_as_skill = lualienvVS, 
+	
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		local damage = data:toDamage()
+		local target = nil
+		local flag = nil
+		if event == sgs.Damage then
+			target = damage.to
+			flag = player:getGender() == target:getGender()
+		elseif event == sgs.Damaged then
+			target = damage.from
+			flag = player:getGender() ~= target:getGender()
+		elseif event == sgs.FinishJudge then
+			local judge = data:toJudge()
+			if judge.reason == self:objectName() then
+				local card = judge.card
+				if card:isBlack() then
+					player:obtainCard(card)
+				else
+					room:askForUseCard(player, "@@lualienv", "@lualienv_prompt")
+				end
+				return false
+			end
+		end
+		if flag then
+			if player:askForSkillInvoke("lualienv") then
+				local judge = sgs.JudgeStruct()
+				judge.good = true
+				judge.reason = "lualienv"
+				judge.who = player
+				room:judge(judge)
+			end
+		end
+		return false
+	end
+}
 sgs.LoadTranslationTable{
-	["testMessage"] = "TTTTTT";
-
 	["yunEX"] = "云EX包",
 	
 	["liyunpeng"] = "李云鹏",
@@ -140,8 +127,8 @@ sgs.LoadTranslationTable{
 	
 	["lualienv"] = "烈女",
 	[":lualienv"] = "每当你受到异性角色造成的一次伤害后，或你对同性角色造成一次伤害后，你可以进行一次判定，若结果为黑色，你获得此牌；若结果为红色，你可以弃置一张牌令一名已受伤的角色回复一点体力。",
-	["@lualienv_prompt"] = "请指定一名已受伤角色，令其回复一点体力。",
-	["~lualienv"] = "\"烈女\"判定结果为红色，你可以弃一张牌（包括装备）令任意一名角色回复一点体力。",
+	["@lualienv_prompt"] = "\"烈女\"判定结果为红色，你可以弃一张牌（包括装备）令任意一名角色回复一点体力。",
+	["~lualienv"] = "请弃一张牌（包括装备）并指定一名已受伤角色。",
 	
 	["EXhuaibeibei"] = "怀贝贝",
 	["&EXhuaibeibei"] = "怀贝贝",
