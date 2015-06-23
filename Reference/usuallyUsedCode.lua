79a5dbf6edfb2a5d553adcd66c9cfb373300f62f
aa3615058 <aa3615058@126.com>
2015-06-23 16:28:52 +0800

    add Trigger Skill

--- Reference/usuallyUsedCode.lua
+++ Reference/usuallyUsedCode.lua
@@ -3,3 +3,16 @@ local msg = sgs.LogMessage()
 msg.type = "#test"
 msg.arg = "DamageCaused!"
 room:sendLog(msg)
+
+--TriggerSkill
+luatest = sgs.CreateTriggerSkill{
+	name = "luatest",
+	frequency = sgs.Skill_NotFrequent,
+	events = {sgs.DamageForseen},
+	can_trigger = function(self, target)
+		return target
+	end,
+	on_trigger = function(self, event, player, data)
+		return false
+	end
+}
